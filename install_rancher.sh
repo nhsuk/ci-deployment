6887ff22d77f87821be7c80dedaf7d6c1707fccc
neil mclaughlin <neil.mclaughlin@digital.nhs.uk>
2017-03-03 09:42:07 +0000

    :apple: Allow OS to be specifified for rancher install

--- install_rancher.sh
+++ install_rancher.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
 declare -r RANCHER_CLI_VERSION='v0.5.0'
+declare OS; OS="$1"
 
 mkdir tmp bin
-wget -qO- https://github.com/rancher/cli/releases/download/${RANCHER_CLI_VERSION}/rancher-linux-amd64-${RANCHER_CLI_VERSION}.tar.gz | tar xvz -C tmp
+curl -sL "https://github.com/rancher/cli/releases/download/${RANCHER_CLI_VERSION}/rancher-${OS}-amd64-${RANCHER_CLI_VERSION}.tar.gz" | tar xvz -C tmp
 mv tmp/rancher-${RANCHER_CLI_VERSION}/rancher bin/rancher
 chmod +x bin/rancher
 rm -r tmp/rancher-${RANCHER_CLI_VERSION}
