0d7ff6da50a7e8a075d5e146505a98a54369caa2
neil mclaughlin <neil.mclaughlin@digital.nhs.uk>
2017-03-06 10:12:06 +0000

    :crossed_flags: Allow script to run on darwin
    Changes to allow the script to be used to deploy rancher CLI
    on dev machines as well as on Travis.
    
    Also:
    
    * OS is now detected so no longer needs to be passed as a parameter
    * allow the install directory to be overriden.

--- install_rancher.sh
+++ install_rancher.sh
@@ -1,11 +1,17 @@
 #!/usr/bin/env bash
 
 declare -r RANCHER_CLI_VERSION='v0.5.0'
-declare OS; OS="$1"
+declare OS; OS=$(uname | tr '[:upper:]' '[:lower:]')
+declare -r URL="https://github.com/rancher/cli/releases/download/${RANCHER_CLI_VERSION}/rancher-${OS}-amd64-${RANCHER_CLI_VERSION}.tar.gz"
+declare BIN="${1:-./bin}"
+declare TMP="./tmp"
 
-mkdir tmp bin
-curl -sL "https://github.com/rancher/cli/releases/download/${RANCHER_CLI_VERSION}/rancher-${OS}-amd64-${RANCHER_CLI_VERSION}.tar.gz" | tar xvz -C tmp
-mv tmp/rancher-${RANCHER_CLI_VERSION}/rancher bin/rancher
-chmod +x bin/rancher
-rm -r tmp/rancher-${RANCHER_CLI_VERSION}
-PATH=$PATH:./bin
+mkdir -p "$TMP" "$BIN"
+
+echo "installing rancher $RANCHER_CLI_VERSION from $URL into $BIN"
+
+curl -sL "$URL" | tar xvz -C "$TMP" 
+mv "$TMP/rancher-${RANCHER_CLI_VERSION}/rancher" "$BIN/rancher"
+chmod +x "$BIN/rancher"
+rm -r "$TMP/rancher-${RANCHER_CLI_VERSION}"
+PATH="$PATH:$BIN"
