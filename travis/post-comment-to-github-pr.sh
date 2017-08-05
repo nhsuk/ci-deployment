0cce0678b404cf9978470c7b5df6f7833ed4cfba
neil mclaughlin <neil.mclaughlin@digital.nhs.uk>
2017-08-05 16:32:08 +0100

    :white_check_mark: Have deployment as a status

--- travis/post-comment-to-github-pr.sh
+++ travis/post-comment-to-github-pr.sh
@@ -11,20 +11,34 @@ fi
 
 # IF SERVICE IS SET TO EXPOSE, APPEND THE URL TO THE MESSAGE
 if [ "$WEB_EXPOSE" = "true" ]; then
-  URL="(http://$DEPLOY_URL)"
+  URL="https://$DEPLOY_URL"
 else
   URL=""
 fi
 
 if [ "$DEPLOYMENT_STATUS" = "successful" ]; then
   MSG=":rocket: deployment of $REPO succeeded $URL"
+  STATUS="success"
 else
   MSG=":boom: deployment of $REPO failed"
+  STATUS="failure"
 fi
 
-PAYLOAD="{\"body\": \"${MSG}\" }"
+PULL_REQUESTS_URL="https://api.github.com/repos/${REPO}/pulls/$PULL_REQUEST"
+echo "$PULL_REQUESTS_URL"
 
-GITHUB_RESPONSE=$(curl -s -o /dev/null -w '%{http_code}' -d "${PAYLOAD}" "https://api.github.com/repos/${REPO}/issues/${PULL_REQUEST}/comments?access_token=${GITHUB_ACCESS_TOKEN}")
+SHA=$(curl -s "$PULL_REQUESTS_URL"| jq --raw-output '.head.sha')
+PAYLOAD="{
+  \"state\": \"${STATUS}\",
+  \"target_url\": \"${URL}\",
+  \"description\": \"Deployment of ${REPO}.\",
+  \"context\": \"Deployment/rancher\"
+}"
+
+echo "${PAYLOAD}" 
+echo "https://api.github.com/repos/${REPO}/statuses/${SHA}"
+
+GITHUB_RESPONSE=$(curl -s -o /dev/null -w '%{http_code}' -d "${PAYLOAD}" "https://api.github.com/repos/${REPO}/statuses/${SHA}?access_token=${GITHUB_ACCESS_TOKEN}")
 
 if [ "${GITHUB_RESPONSE}" = "201" ]; then
   echo "Comment '${MSG}' added to pr ${PULL_REQUEST} on ${REPO}"
