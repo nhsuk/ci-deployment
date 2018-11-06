c39040bfc9d2e9c12f3d350b0d50d8dfca01bcf5
George MacKay-Shore <george.mackay-shore@bjss.com>
2018-11-06 12:30:22 +0000

    Switch quotes

--- gitlab/answers/30-prototype.sh
+++ gitlab/answers/30-prototype.sh
@@ -1,8 +1,7 @@
 if [ "$PROTOTYPE" == "TRUE" ]; then
   {
-    host="${RANCHER_STACK_NAME//_/-}.${TRAEFIK_DOMAIN}"
     echo "DB_TYPE=sqlite"
-    echo 'TRAEFIK_RULE="Host: ${host}"'
-    echo "HOST_BETA=${host}"
+    echo "TRAEFIK_RULE='Host: ${RANCHER_STACK_NAME//_/-}.${TRAEFIK_DOMAIN}'"
+    echo "HOST_BETA=${RANCHER_STACK_NAME//_/-}.${TRAEFIK_DOMAIN}"
   } >> answers.txt
 fi
