290844eef2d7e593360d775e82b38dce07eadcc2
George MacKay-Shore <george.mackay-shore@bjss.com>
2018-11-06 11:36:31 +0000

    Add more rules for prototypes

--- gitlab/answers/30-prototype.sh
+++ gitlab/answers/30-prototype.sh
@@ -1,6 +1,8 @@
 if [ "$PROTOTYPE" == "TRUE" ]; then
   {
+    host="${RANCHER_STACK_NAME//_/-}.${TRAEFIK_DOMAIN}"
     echo "DB_TYPE=sqlite"
-    echo 'TRAEFIK_RULE="Host: ${RANCHER_STACK_NAME}.${TRAEFIK_DOMAIN}"'
+    echo 'TRAEFIK_RULE="Host: ${host}"'
+    echo "HOST_BETA=${host}"
   } >> answers.txt
 fi
