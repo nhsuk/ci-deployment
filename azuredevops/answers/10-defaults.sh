97bfaa4cb3d5d04de8886d366b7c0e0190184671
Neil Moran <neil.moran@nhs.net>
2018-12-07 11:32:46 +0000

    Remove branch-specific vars for AZDO build

--- azuredevops/answers/10-defaults.sh
+++ azuredevops/answers/10-defaults.sh
@@ -13,14 +13,6 @@
 if [ -n "${BUILD_BUILDNUMBER}" ]; then
   echo "DOCKER_IMAGE_TAG=${BUILD_BUILDNUMBER}" >> answers.txt
 # INTEGRATION BUILD
-elif [ "$BUILD_SOURCEBRANCHNAME" = "master" ]; then
+else [ "$BUILD_SOURCEBRANCHNAME" = "master" ]; then
   echo "DOCKER_IMAGE_TAG=latest" >> answers.txt
-else
-  # IT'S A BRANCH
-  {
-    echo "DEPLOY_URL='${SYSTEM_TEAMPROJECT}-${BUILD_SOURCEBRANCHNAME}.dev.beta.nhschoices.net'"
-    echo "RANCHER_STACK_NAME='${SYSTEM_TEAMPROJECT}-${BUILD_SOURCEBRANCHNAME}'"
-    echo "DOCKER_IMAGE_TAG=${BUILD_SOURCEBRANCHNAME}"
-    echo "RANCHER_DESCRIPTION='(${BUILD_SOURCEBRANCHNAME}) (https://dev.azure.com/nhsuk/${SYSTEM_TEAMPROJECT})'"
-  } >> answers.txt
 fi
