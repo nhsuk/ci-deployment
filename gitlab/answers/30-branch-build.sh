9d024ff142856016dbc6a4e76b4e30aabfb7b556
George MacKay-Shore <george.mackay-shore@bjss.com>
2019-01-18 12:41:41 +0000

    Push prorotype to singular domain

--- gitlab/answers/30-branch-build.sh
+++ gitlab/answers/30-branch-build.sh
@@ -1,6 +1,10 @@
 if [ "$BRANCH_BUILD" == "TRUE" ]; then
   {
-    host="${CI_PROJECT_NAME//_/-}-${CI_COMMIT_REF_SLUG//_/-}"
+    branch="${CI_COMMIT_REF_SLUG//_/-}"
+    if [ "${branch:0:5}" == "proto" ]; then
+      branch="prototype"
+    fi
+    host="${CI_PROJECT_NAME//_/-}-${branch}"
     domain="dev.beta.nhschoices.net"
     echo "DB_TYPE=sqlite"
     echo "TRAEFIK_RULE='Host: ${host}.${domain}'"
